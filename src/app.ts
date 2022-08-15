2108c01720f75ceb5fb010ff7964b8c41272f946
kashifkhan0336 <kashifkhan0336@gmail.com>
2022-08-15 10:23:40 +0500

    hono

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
-import express, { Express, Request, Response } from "express";
+import * as dotenv from "dotenv";
 
+const cors = require("cors");
+dotenv.config();
+// export interface ProcessEnv {
+//   [key: string]: string | undefined;
+// }
+import express, { Express, Request, Response, json } from "express";
+//console.log(process.env.PORT);
 const app: Express = express();
-const port: Number = 5000;
+const port: Number = parseInt(process.env.PORT as string) || 5000;
+app.use(cors());
+app.use(json());
 app.get("/", (request: Request, response: Response) => {
+  response.json({ status: "200 Ok" });
+});
+
+app.post("/register", (request: Request, response: Response) => {
+  console.log(request.body);
   response.json({ status: "200 Ok" });
 });
 
